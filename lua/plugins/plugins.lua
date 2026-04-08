4a5a8a96f264ad2b1c1f32324285b1bafb33996a
Zhenyu Zhang <ashfish.zzy@protonmail.com>
2026-04-08 14:54:06 -0400

    update typst

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,24 @@
 return {
+  --markdown plugin
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {},
+  },
+  --latex plugin
+  {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end,
+  },
+
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "1.*",
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
   },
 }
