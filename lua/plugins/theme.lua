fe98c8101fbe5c640f85877a0921a05d9a2baf57
ZhenyuZhang-Sys <zhenyu.zhang@duke.edu>
2026-05-04 21:49:36 +0000

    update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,16 @@
 -- github theme
 
 return {
-  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+
+  -- Using Lazy
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load()
+    end,
+  },
 }
